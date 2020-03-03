86cb6576d353a5c328e9b0439a462e2a73d18293
Bruno Germano <bruno.germano@gmail.com>
2020-03-02 21:53:55 -0300

    update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,42 +1,34 @@
-import React from 'react';
-import { FaGithub } from 'react-icons/fa';
-import { StaticQuery, graphql } from 'gatsby';
-import './style.scss';
-// import Emoji from './emoji';
+import React from "react";
+import { StaticQuery, graphql } from "gatsby";
+import "./style.scss";
 
 const Footer = () => (
-	<StaticQuery
-		query={graphql`
-			query SocialQuery {
-				site {
-					siteMetadata {
-						twitter
-						medium
-						github
-					}
-				}
-			}
-		`}
-		render={data => (
-			<footer className="footer center has-background-light">
-				<div className="content has-text-centered">
-					<article className="media center">
-
-						<span className="icon">
-							<a href="https://github.com/app-generator/gatsby-starter-bulma-css">
-								<FaGithub size="fa-2x" color="black" />
-							</a>
-						</span>
-
-					</article>
-					<br />
-					<p className="is-size-6">
-						
-					</p>
-				</div>
-			</footer>
-		)}
-	/>
+  <StaticQuery
+    query={graphql`
+      query SocialQuery {
+        site {
+          siteMetadata {
+            twitter
+            medium
+            github
+          }
+        }
+      }
+    `}
+    render={data => (
+      <footer className="footer center has-background-dark">
+        <div className="content has-text-centered">
+          <div className="columns">
+            <div className="column">
+              <p className="has-text-light">
+                Todos os direitos reservados &copy; O Bruno Germano
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    )}
+  />
 );
 
 export default Footer;
