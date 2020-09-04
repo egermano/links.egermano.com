0f24b1de7e5da52190ad52e183263c98005af6d2
Bruno Germano <bruno.germano@gmail.com>
2020-09-04 11:07:46 -0300

    update env

--- src/service/youtube-helper.js
+++ src/service/youtube-helper.js
@@ -3,7 +3,7 @@ const { default: Axios } = require("axios");
 exports.lastVideo = async () => {
     const channelId = 'UCBWbWViVqDHckknir8PIIdg';
     try {
-        const response = await Axios.get(`http://localhost:3000/localhost/v1/last-video`, {
+        const response = await Axios.get(process.env.GATSBY_YT_HELPER_URL , {
             headers: {
                 'Content-Type':'application/json',
             },
@@ -11,7 +11,7 @@ exports.lastVideo = async () => {
                 channelId
             },
         });
-    
+        
         return response.data;
     } catch (error) {
         console.error(error);
