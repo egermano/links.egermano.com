4fd31cff33d236d8e793a6826d1afb579ecbbd61
Bruno Germano <bruno.germano@gmail.com>
2020-09-04 10:40:40 -0300

    add config loader

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+	path: `.env${process.env.NODE_ENV?`.${process.env.NODE_ENV}`:''}`,
+})
+  
 module.exports = {
 	siteMetadata: {
 		title: 'O Bruno Germano - Meus Links',
